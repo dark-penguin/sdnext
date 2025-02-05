86fd203fdf10802c89e571127116174281749315
darkpenguin <darkpenguin@posteo.de>
2025-02-05 16:34:05 +0400

    Add --insecure to commandline arguments

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 export SD_PIP_DEBUG=true
 export SD_INSTALL_DEBUG=true
 
-LD_PRELOAD=libtcmalloc.so.4 ./webui.sh --debug --listen --use-rocm --quick
+LD_PRELOAD=libtcmalloc.so.4 ./webui.sh --debug --listen --use-rocm --insecure --quick
