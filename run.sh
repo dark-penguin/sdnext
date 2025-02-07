031f6c334adcce4a990a8246beeb288e290e137a
darkpenguin <darkpenguin@posteo.de>
2025-02-07 18:17:37 +0400

    Extract directories into an .env

--- run.sh
+++ run.sh
@@ -2,11 +2,11 @@
 
 cd "$(dirname "$0")"
 
-export SD_DATADIR="/ssd/sd_data"
-export SD_MODELSDIR="/ssd/sd_models"
+. .env
 
-export SD_PIP_DEBUG=true
-export SD_INSTALL_DEBUG=true
+# Check that directories are set
+[ -z "$SD_DATADIR" ] && echo "SD_DATADIR not set! (check your .env!)" >&2 && exit 1
+[ -z "$SD_MODELSDIR" ] && echo "SD_MODELSDIR not set! (check your .env!)" >&2 && exit 1
 
 export LD_PRELOAD=libtcmalloc.so.4
-./webui.sh --debug --listen --use-rocm --insecure --quick --skip-all
+./webui.sh --debug --listen --use-rocm --insecure --quick
